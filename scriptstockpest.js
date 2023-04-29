24cf7aaaf308ff8ffd1827db7249a7e3e3ff835b
deekshith2929 <puvvadideekshith2@gmail.com>
2023-04-29 13:48:56 +0530

    stamp with/without update

--- scriptstockpest.js
+++ scriptstockpest.js
@@ -1,6 +1,7 @@
 function calculate(){
     localStorage.setItem("fromdate",document.getElementById("fromdate").value);
     localStorage.setItem("todate",document.getElementById("todate").value);
+    localStorage.setItem("stamp",document.getElementById("stamp").value);
     for(var i=1;i<31;i++){
     localStorage.setItem("products"+i,document.getElementById("products"+i).value);
     localStorage.setItem("productstext"+i,document.getElementById("productstext"+i).value);
